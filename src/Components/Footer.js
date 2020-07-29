d6922113dcd54dfc4365a91eca49d6e5e4938cc8
Tummala Krishna Kishore <tummala.krishna-kishore@capgemini.com>
2020-07-29 12:00:06 +0530

    Updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2019 Krishna Kishore</li>
+              <li>&copy; Copyright 2020 Krishna Kishore</li>
               {/* <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
            </ul>
 
