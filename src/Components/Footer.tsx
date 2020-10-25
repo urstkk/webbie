eff23e30a872d84213a5902e297e8006fda98207
Krishna Kishore <krishna30kishore@gmail.com>
2020-10-25 21:55:10 +0530

    Link view count

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -8,6 +8,9 @@ class Footer extends Component<IProps, IState> {
       var networks= this.props.data.social.map((network:any)=>{
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
+      var pageViewCounts = this.props.data.PageViewCount.map((lCount:any)=>{
+        return <img src={lCount.viewCountUrl} alt="Hits"></img>
+      })
     }
 
     return (
@@ -21,10 +24,12 @@ class Footer extends Component<IProps, IState> {
 
            <ul className="copyright">
               <li>&copy; Copyright 2020 Krishna Kishore</li>
+              <li>{pageViewCounts}</li>
               {/* <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
            </ul>
 
         </div>
+        
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
      </div>
   </footer>
