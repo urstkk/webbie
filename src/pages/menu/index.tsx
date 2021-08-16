d51300e5ae606552499954e057e312e16885f0cd
Tummala Krishna Kishore <tummala.krishna-kishore@capgemini.com>
2021-08-16 18:59:11 +0530

    cleaning code smells

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,13 +1,11 @@
 import { motion } from 'framer-motion';
 import Head from 'next/head';
-import React, { useMemo } from 'react';
+import React from 'react';
 
 import withPageTransition from '../../components/hoc/with-page-transition';
 import { navVariants } from '../../components/menu/Menu.animation';
-import { addCamelCaseKeys } from '../../lib';
 import { GA_TRACKING_ID } from '../../lib/ga';
 import InfoSection from './info-section/InfoSection';
-import s from './Menu.module.scss';
 import TileSection from './tile-section/TileSection';
 
 /**
