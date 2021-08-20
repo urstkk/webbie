c6290f43291d2907dd2a1543d66ebbdee3c41ddb
Tummala Krishna Kishore <tummala.krishna-kishore@capgemini.com>
2021-08-20 09:15:35 +0530

    changes to test

--- __tests__/snapshots.tsx
+++ __tests__/snapshots.tsx
@@ -6,6 +6,6 @@ import Index from '../src/pages/index';
 describe('With Enzyme', function () {
   it('App shows "A simple example repo" in a <p> tag', function () {
     const app = shallow(<Index />);
-    expect(app.find('p').text()).toEqual('A simple example repo');
+    expect(app.find('p').text()).equal('A simple example repo');
   });
 });
