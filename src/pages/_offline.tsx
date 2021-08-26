5655c1fe5b0be92bbef860542d041d8f12cafdb8
Tummala Krishna Kishore <tummala.krishna-kishore@capgemini.com>
2021-08-26 14:07:58 +0530

    changes to logo and offline text

--- src/pages/_offline.tsx
+++ src/pages/_offline.tsx
@@ -1,4 +1,26 @@
+import { motion } from 'framer-motion';
 import Head from 'next/head';
+import React from 'react';
+
+import Page from '../components/page/Page';
+import PageBody from '../components/page-body/PageBody';
+import PageHeader from '../components/page-header/PageHeader';
+
+/**
+ * Animation variants for the offline container element.
+ */
+const offlineContainerVariants = {
+  initial: {
+    opacity: 0
+  },
+  enter: {
+    delay: 0.6,
+    opacity: 1,
+    transition: {
+      when: 'beforeChildren'
+    }
+  }
+};
 
 const MyOffline: React.FC = () => {
   return (
@@ -6,8 +28,18 @@ const MyOffline: React.FC = () => {
       <Head>
         <title>KK&apos;s Portfolio</title>
       </Head>
-      <h1>You are currently accessing Application in Offline</h1>
-      <h2>Please be patient and try again</h2>
+      <Page>
+        <PageHeader
+          title="Posts"
+          description="My ramblings about things that excite me. Might take some time to read but will definitely be worth your time."
+        />
+        <PageBody>
+          <motion.div variants={offlineContainerVariants}>
+            <h2>You are currently accessing Application in Offline</h2>
+            <h2>Please be patient and try again</h2>
+          </motion.div>
+        </PageBody>
+      </Page>
     </>
   );
 };
