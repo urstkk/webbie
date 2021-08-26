1d02ab31522b1883b194dc56a27de7cf72b2bc47
Tummala Krishna Kishore <tummala.krishna-kishore@capgemini.com>
2021-08-26 13:15:19 +0530

    adding offline fallback

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,30 @@ class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name="application-name" content="KK's Portfolio" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="KK's Portfolio" />
+          <meta
+            name="description"
+            content="Krishna Kishore's portfolio and blog where you can get to know about him, what he is upto and pick up a trick or two from his blog posts which are mostly about web development."
+          />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#fd8111" />
+          {/* 
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          /> */}
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
         <body className={'font-sans text-base text-white bg-dark'}>
           <Main />
           <NextScript />
